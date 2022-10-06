684bea33022bc480c34c6f4fd6040cc066e113cd
cwu90 <canny.wu90@gmail.com>
2022-10-06 10:41:07 -0400

    Commit #1

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { useContext } from 'react';
 import Feedbackitem from './Feedbackitem';
 import FeedbackContext from '../context/FeedbackContext';
+import Spinner from './shared/Spinner';
 
 //feedback is the data state name from the App()
-function FeedbackList({}) {
-  const { feedback } = useContext(FeedbackContext);
+function FeedbackList() {
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet</p>;
   }
 
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       {feedback.map(item => (
         <Feedbackitem key={item.id} item={item} />
